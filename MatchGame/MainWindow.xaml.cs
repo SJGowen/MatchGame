a87cb1a7c567567cfd5205361c1d4552948d1a40
Stephen Gowen <stephen.gowen@cam-systems.co.uk>
2021-03-11 12:11:54 +0000

    Made the Game more like a match game, as in do not display pictures until the user clicks on them. Involved using a second DispatcherTimer called delay and on the firing of this event changed the emoji's displayed to either a tick if emoji's matched or a ? if they don't. I turn the delay timer off, when the user clicks on any box; this is incase the user is quicker than the one second delay that I start when the user has made their second choice. This is so that user has a chance to see what second emoji is.

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace MatchGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         readonly DispatcherTimer timer = new();
-        int tenthsOfSecondsElapsed;
-        int matchesFound;
+        readonly DispatcherTimer delay = new();
+        private List<string> gameEmoji = new();
+        private TextBlock findingMatchFalseClicked;
+        private TextBlock findingMatchTrueClicked;
+        private int tenthsOfSecondsElapsed;
+        private int matchesFound;
+        private bool displayMatch = false;
+        private bool findingMatch = false;
+
 
         public MainWindow()
         {
@@ -31,6 +27,9 @@ namespace MatchGame
 
             timer.Interval = TimeSpan.FromSeconds(.1);
             timer.Tick += Timer_Tick;
+
+            delay.Interval = TimeSpan.FromSeconds(1);
+            delay.Tick += Delay_Tick;
             SetUpGame();
         }
 
@@ -45,8 +44,14 @@ namespace MatchGame
             }
         }
 
+        private void Delay_Tick(object sender, EventArgs e)
+        {
+            ProcessDelay();
+        }
+
         private void SetUpGame()
         {
+            gameEmoji.Clear();
             List<string> animalEmoji = new()
             {
                 "🐘", "🐘",
@@ -61,42 +66,76 @@ namespace MatchGame
 
             Random random = new();
 
-            foreach (var textBlock in mainGrid.Children.OfType<TextBlock>().Where(x => x.Name != "timeTextBlock"))
+            foreach (var textBlock in mainGrid.Children.OfType<TextBlock>().Where(x => x.Name == ""))
             {
-                textBlock.Visibility = Visibility.Visible;
+                textBlock.Text ="?";
                 int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
+                gameEmoji.Add(animalEmoji[index]);
                 animalEmoji.RemoveAt(index);
             }
-
-            timer.Start();
-            tenthsOfSecondsElapsed = 0;
-            matchesFound = 0;
         }
 
-        TextBlock lastBlockClicked;
-        bool findingMatch = false;
 
         private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            void ProcessClick(TextBlock textBlock, string emojiClicked, bool match)
+            {
+                textBlock.Text = emojiClicked;
+                displayMatch = match;
+                findingMatchTrueClicked = textBlock;
+                findingMatch = false;
+                delay.Start();
+            }
+
+            ProcessTimerEvents();
             TextBlock textBlock = sender as TextBlock;
-            if (findingMatch == false)
+
+            if (textBlock.Text == "?")
             {
-                textBlock.Visibility = Visibility.Hidden;
-                lastBlockClicked = textBlock;
-                findingMatch = true;
+                var pos = int.Parse(textBlock.Tag.ToString());
+                string emojiClicked = gameEmoji[pos];
+
+                if (findingMatch == false)
+                {
+                    textBlock.Text = emojiClicked;
+                    findingMatchFalseClicked = textBlock;
+                    findingMatch = true;
+                }
+                else if (emojiClicked == findingMatchFalseClicked.Text)
+                {
+                    matchesFound++;
+                    ProcessClick(textBlock, emojiClicked, true);
+                }
+                else
+                {
+                    ProcessClick(textBlock, emojiClicked, false);
+                }
             }
-            else if (textBlock.Text == lastBlockClicked.Text)
+        }
+
+        private void ProcessTimerEvents()
+        {
+            ProcessStart();
+            ProcessDelay();
+        }
+
+        private void ProcessStart()
+        {
+            if (!timer.IsEnabled)
             {
-                matchesFound++;
-                textBlock.Visibility = Visibility.Hidden;
-                findingMatch = false;
+                timer.Start();
+                tenthsOfSecondsElapsed = 0;
+                matchesFound = 0;
             }
-            else
+        }
+
+        private void ProcessDelay()
+        {
+            if (delay.IsEnabled)
             {
-                lastBlockClicked.Visibility = Visibility.Visible;
-                findingMatch = false;
+                delay.Stop();
+                findingMatchFalseClicked.Text = displayMatch ? "✔" : "?";
+                findingMatchTrueClicked.Text = displayMatch ? "✔" : "?";
             }
         }
 
@@ -104,6 +143,7 @@ namespace MatchGame
         {
             if (matchesFound == 8)
             {
+                timer.Stop();
                 SetUpGame();
             }
         }
